79abdc1ef31e996bfd98acb6a50a4966d74994cd
Abdullah Suleiman(Manu Jr.) <manujr.dev@gmail.com>
2026-05-05 17:29:46 +0100

    added styling and structure to the note area and js events to control cursor and selection

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const sidebarToggle = document.getElementById("toggle-sidebar")
 const sidebar = document.getElementById("sidebar")
+const title = document.getElementById("note-title")
+
+
+const range = document.createRange()
+range.selectNodeContents(title)
+
+const selection = window.getSelection()
+selection.removeAllRanges()
+selection.addRange(range)
 
 sidebarToggle.addEventListener("click", ()=> {
     sidebar.classList.toggle("open")
