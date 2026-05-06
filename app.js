22d1f033d5efb4d73563d16867ff8a3e94dfa338
Abdullah Suleiman(Manu Jr.) <manujr.dev@gmail.com>
2026-05-06 19:36:09 +0100

    added transition to the editor to match the side bar and also prevent default action when enter is pressed in the title and focuses on the body instead

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const sidebarToggle = document.getElementById("toggle-sidebar")
 const sidebar = document.getElementById("sidebar")
+const editor = document.getElementById("editor")
 const title = document.getElementById("note-title")
+const body = document.getElementById("note-body")
 
 
 const range = document.createRange()
@@ -10,8 +12,17 @@ const selection = window.getSelection()
 selection.removeAllRanges()
 selection.addRange(range)
 
+title.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault()
+        body.focus()
+
+    }
+})
+
 sidebarToggle.addEventListener("click", ()=> {
     sidebar.classList.toggle("open")
+    editor.classList.toggle("open")
     sidebarToggle.classList.toggle("on")
     console.log("clicked")
 })
