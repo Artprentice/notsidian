f4d3476df6c0c21222093925c929ce75cac61dd9
Abdullah Suleiman(Manu Jr.) <manujr.dev@gmail.com>
2026-05-07 09:46:16 +0100

    added a new notes button and fix the sidebar toggle glitching during animation

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const sidebar = document.getElementById("sidebar")
 const editor = document.getElementById("editor")
 const title = document.getElementById("note-title")
 const body = document.getElementById("note-body")
+const newNote = document.getElementById("new-note")
 
 const savedTitle = localStorage.getItem('notsidian_title')
 const savedBody = localStorage.getItem('notsidian_body')
+
+const notes = {}
 
 
 savedTitle ? title.innerText = savedTitle : title.innerText = "yup"
@@ -26,14 +29,37 @@ title.addEventListener("keydown", (e) => {
     }
 })
 
-sidebarToggle.addEventListener("click", ()=> {
+sidebarToggle.addEventListener("click", () => {
     sidebar.classList.toggle("open")
     editor.classList.toggle("open")
+    sidebarToggle.style.display = "none"
     sidebarToggle.classList.toggle("on")
     console.log("clicked")
+})
+
+sidebar.addEventListener("transitionend", () => {
+    if (!sidebar.classList.contains("open")) {
+        sidebarToggle.style.display = "block"
+    }
+})
+
+newNote.addEventListener("click", () => {
+    title.innerText = "Untitled"
+    body.innerText = ""
+    sidebar.classList.toggle("open")
+    editor.classList.toggle("open")
+    sidebar.classList.toggle("on")
+    sidebarToggle.classList.toggle("on")
 })
 
 body.addEventListener("input", () => {
     localStorage.setItem('notsidian_title', title.innerText)
     localStorage.setItem('notsidian_body', body.innerText)
+})
+
+document.addEventListener("click", (e) => {
+    if (!sidebar.contains(e.target) && e.target !== sidebarToggle){
+        sidebar.classList.remove("open")
+        editor.classList.remove("open")
+    }
 })
